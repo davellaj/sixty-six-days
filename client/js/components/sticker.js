aad6d649213650cfa8100d37665a987e5bc0d344
trig0 <allengordon0@gmail.com>
2016-12-22 11:39:24 -0600

    added img height

--- client/js/components/sticker.js
+++ client/js/components/sticker.js
@@ -4,8 +4,8 @@ function Sticker() {
   return (
     <div>
       <img height={100} className="sticker" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT0hCinHGYyY6xo2azGQVC2udeCPFWcvCSnDteoJ15ifC3qcNOu" alt="dog" />
-      <img className="sticker" src="https://ih1.redbubble.net/image.12893531.1313/sticker,220x200-pad,220x200,ffffff.u1.jpg" />
-      <img className="sticker" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRa1diPgUV58TD_nHuICVMAvpS3NWbQM-UamR_nTYSDETEjtErWGQ" />
+      <img height={100} className="sticker" src="https://ih1.redbubble.net/image.12893531.1313/sticker,220x200-pad,220x200,ffffff.u1.jpg" />
+      <img height={100} className="sticker" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRa1diPgUV58TD_nHuICVMAvpS3NWbQM-UamR_nTYSDETEjtErWGQ" />
     </div>
   )
 }
