c06e7bdaf4ad031c0f8ccf08d381ed0e3add421b
davellaj <jamie.davella@gmail.com>
2016-12-22 12:02:07 -0500

    add navBar

---
+++ client/js/components/navBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+      <a href="">Home</a>
+      &nbsp;
+      <a href="">Profile</a>
+      &nbsp;
+      <a href="">Friends</a>
+    </div>
+  )
+}
+
+export default NavBar
