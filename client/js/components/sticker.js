c75544a3465d42ba7ba53936ca4733c4b02d00d5
davellaj <jamie.davella@gmail.com>
2016-12-21 23:14:06 -0500

    add extra stickers

--- client/js/components/sticker.js
+++ client/js/components/sticker.js
@@ -3,9 +3,41 @@ import React from 'react'
 function Sticker() {
   return (
     <div>
-      <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT0hCinHGYyY6xo2azGQVC2udeCPFWcvCSnDteoJ15ifC3qcNOu" alt="dog" />
+      <img className="sticker" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT0hCinHGYyY6xo2azGQVC2udeCPFWcvCSnDteoJ15ifC3qcNOu" alt="dog" />
+      <img className="sticker" src="https://ih1.redbubble.net/image.12893531.1313/sticker,220x200-pad,220x200,ffffff.u1.jpg" />
+      <img className="sticker" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRa1diPgUV58TD_nHuICVMAvpS3NWbQM-UamR_nTYSDETEjtErWGQ" />
     </div>
   )
 }
 
 export default Sticker
+
+
+
+
+// <div className="row">
+//    <div className="col-md-4">
+//        <div className="thumbnail">
+//            <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT0hCinHGYyY6xo2azGQVC2udeCPFWcvCSnDteoJ15ifC3qcNOu" alt="Lights" style="width:100%">
+//            <div className="caption">
+//              <p>Lorem ipsum...</p>
+//            </div>
+//        </div>
+//     </div>
+//        <div className="col-md-4">
+//          <div className="thumbnail">
+//              <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT0hCinHGYyY6xo2azGQVC2udeCPFWcvCSnDteoJ15ifC3qcNOu" alt="Nature" style="width:100%">
+//              <div className="caption">
+//                <p>Lorem ipsum...</p>
+//              </div>
+//          </div>
+//        </div>
+//      <div className="col-md-4">
+//        <div className="thumbnail">
+//            <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT0hCinHGYyY6xo2azGQVC2udeCPFWcvCSnDteoJ15ifC3qcNOu" style="width:100%">
+//            <div className="caption">
+//              <p>Lorem ipsum...</p>
+//            </div>
+//        </div>
+//    </div>
+// </div>
