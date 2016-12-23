19e983d1d61a0975b79f2463ba3a581668f822ca
trig0 <allengordon0@gmail.com>
2016-12-22 22:29:31 -0600

    compare to master

--- client/js/components/goals.js
+++ client/js/components/goals.js
@@ -4,72 +4,63 @@ import * as actions from '../actions/actions';
 import Calendar from './due-dates';
 
 export class Goals extends React.Component {
-  constructor(props){
-    super(props);
-    this.sendUserGoal = this.sendUserGoal.bind(this);
-    // this.saveUserGoal = this.saveUserGoal.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.sendUserGoal = this.sendUserGoal.bind(this);
+        // this.saveUserGoal = this.saveUserGoal.bind(this);
+    }
 
-  componentDidMount() {
-    this.props.dispatch(actions.fetchGoals())
-  }
+    componentDidMount() {
+        this.props.dispatch(actions.fetchGoals())
+    }
 
+    sendUserGoal(event) {
+        event.preventDefault();
+        const userGoal = this.textInput.value;
+        console.log('fired off sendUserGoal event', userGoal)
+        this.props.dispatch(actions.addGoal(userGoal))
+        this.textInput.value = '';
+    }
 
-  sendUserGoal(event){
-    event.preventDefault();
-    const userGoal = this.textInput.value;
-    console.log('fired off sendUserGoal event', userGoal)
-    this.props.dispatch(actions.addGoal(userGoal))
-    this.textInput.value='';
-  }
-
-  // saveUserGoal(event){
-  //   const newUserGoal = event.target.innerText;
-  //   console.log(newUserGoal);
+    // saveUserGoal(event){
+    //   const newUserGoal = event.target.innerText;
+    //   console.log(newUserGoal);
     // console.log(event.target.id[0]);
     // this.props.dispatch(actions.updateGoal(newUserGoal))
-  // }
-  render() {
-    // console.log(this.props.userGoals);
-    const goals = this.props.userGoals.map((goal, idx) => {
-      return
-      <div>
-      <div className="goalBox" key={idx}>
-        <div key={idx} className="goalText"
-          onBlur={(event) => this.props.dispatch(actions.updateGoal(event.target.innerText, goal._id))}
-           contentEditable='true'>{goal.goal}
-         </div>
-         <button className="btn-xs btn-warning"
-           onClick={() => {this.props.dispatch(actions.deleteGoal(goal._id))}}>
-          Delete</button>
-        </div>
-          <Calendar goal={goal.goal}/>
-      
-    </div>
-    })
-
-    return (
-      <div className="container">
-        <form className="form-horizontal" onSubmit={this.sendUserGoal}>
-          <div className="form-group has-success has-feedback">
-            <label className="col-sm-2 control-label" for="inputSuccess">New Goal</label>
-            <div className="col-sm-6">
-              <input type="text" className="form-control" id="inputSuccess" ref={input => this.textInput = input}
-                placeholder="type your goal and press enter"/>
+    // }
+    render() {
+        // console.log(this.props.userGoals);
+        const goals = this.props.userGoals.map((goal, idx) => {
+            return <div className="goalBox" key={idx}>
+                <div key={idx} className="goalText" onBlur={(event) => this.props.dispatch(actions.updateGoal(event.target.innerText, goal._id))} contentEditable='true'>{goal.goal}
+                </div>
+                <button className="btn-xs btn-warning" onClick={() => {
+                    this.props.dispatch(actions.deleteGoal(goal._id))
+                }}>
+                    Delete</button>
+                <Calendar goal={goal.goal}/>
             </div>
-          </div>
-        </form>
-        <div className="goalsContainer">
-          {goals}
-        </div>
-        </div>
+        })
 
-    )
-  }
+        return (
+            <div className="container">
+                <form className="form-horizontal" onSubmit={this.sendUserGoal}>
+                    <div className="form-group has-success has-feedback">
+                        <label className="col-sm-2 control-label" for="inputSuccess">New Goal</label>
+                        <div className="col-sm-6">
+                            <input type="text" className="form-control" id="inputSuccess" ref={input => this.textInput = input} placeholder="type your goal and press enter"/>
+                        </div>
+                    </div>
+                </form>
+                <div className="goalsContainer">
+                    {goals}
+                </div>
+            </div>
+
+        )
+    }
 }
 
-const mapStateToProps = (state, props) => ({
-  userGoals: state.goals
-})
+const mapStateToProps = (state, props) => ({userGoals: state.goals})
 
 export default connect(mapStateToProps)(Goals)
--- client/js/components/pageContainer.js
+++ client/js/components/pageContainer.js
@@ -4,20 +4,21 @@ import Sticker from './sticker'
 import NavBar from './navBar'
 
 function Page() {
-  return (
-    <div className="container">
-      <NavBar />
-        <h1 className="title">66 Days</h1>
-        <hr></hr>
-        <p className="subtitle">On average, it takes more than two months before a new behavior becomes automatic — 66 days to be exact.</p>
-        <h3>Your Goals</h3>
-      <Goals />
-      <div className="jumbotron">
-        <h3>Stickers</h3>
-        <Sticker />
-      </div>
-    </div>
-  )
+    return (
+        <div className="container">
+            <NavBar/>
+            <h1 className="title">66 Days</h1>
+            <h2 className="subtitle">Set goals, stick to them.</h2>
+            <hr></hr>
+            <p className="66">On average, it takes more than two months before a new behavior becomes automatic — 66 days to be exact.</p>
+            <h3>Your Goals</h3>
+            <Goals/>
+            <div className="jumbotron">
+                <h3>Stickers</h3>
+                <Sticker/>
+            </div>
+        </div>
+    )
 }
 
 export default Page
